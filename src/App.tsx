8dc74a5f3a7dac00a29af29d5ab7a3a7eefb639c
Kush Mishra <kushmishra0598@gmail.com>
2025-12-04 00:38:11 +0530

    again updated to fit hopefully works

--- src/App.tsx
+++ src/App.tsx
@@ -803,6 +803,8 @@ export default function RiverFrontCafe() {
                 {/* <p className="text-gray-600">https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1058.2825056503802!2d80.97200531410353!3d26.851913384725535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd268b7e93df%3A0xe1dc57a80c8f5410!2sRiver%20Front%20Cafe!5e0!3m2!1sen!2sin!4v1764787889575!5m2!1sen!2sin</p> */}
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1058.2825056503802!2d80.97200531410353!3d26.851913384725535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd268b7e93df%3a0xe1dc57a80c8f5410!2sriver%20front%20cafe!5e0!3m2!1sen!2sin!4v1764787889575!5m2!1sen!2sin"
+                  width="100%"
+                  height="100%"
                   loading="lazy"
                 ></iframe>
               </div>

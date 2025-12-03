a938a0c86fd1fb03513a02c767e23cc04246bd22
Kush Mishra <kushmishra0598@gmail.com>
2025-12-04 00:31:52 +0530

    again

--- src/App.tsx
+++ src/App.tsx
@@ -800,13 +800,13 @@ export default function RiverFrontCafe() {
                 Find Us on Map
               </h3>
               <div className="bg-gray-200 h-64 rounded-lg flex items-center justify-center">
-                {/* <p className="text-gray-600">https://maps.app.goo.gl/KBeHo6R2DmnzQwD86</p> */}
-                <iframe
+                <p className="text-gray-600">https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1058.2825056503802!2d80.97200531410353!3d26.851913384725535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd268b7e93df%3A0xe1dc57a80c8f5410!2sRiver%20Front%20Cafe!5e0!3m2!1sen!2sin!4v1764787889575!5m2!1sen!2sin</p>
+                {/* <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1058.2825056503802!2d80.97200531410353!3d26.851913384725535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfd268b7e93df%3A0xe1dc57a80c8f5410!2sRiver%20Front%20Cafe!5e0!3m2!1sen!2sin!4v1764787889575!5m2!1sen!2sin"
-                  width="600"
+                  width="800"
                   height="450"
                   loading="lazy"
-                ></iframe>
+                ></iframe> */}
               </div>
             </div>
           </div>
